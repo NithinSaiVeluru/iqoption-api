b56077cc69eee660633d53b8222193825f86d23d
vns1111 <veluruns@gmail.com>
2025-09-26 12:06:49 +0530

    Update app.py

--- app.py
+++ app.py
@@ -20,3 +20,7 @@ def trade():
         "received_signal": signal,
         "message": f"Executed {signal} trade (simulation for now)"
     })
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
+
+    
